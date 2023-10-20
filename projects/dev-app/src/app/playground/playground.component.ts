971b0b795129652edd3a37d3d70a95608a39129b
nzbin <nzb329@163.com>
2023-10-20 23:24:42 +0800

    feat(dev-app): add playground

--- projects/dev-app/src/app/playground/playground.component.ts
+++ projects/dev-app/src/app/playground/playground.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { GuiConfigs, GuiModule } from '@acrodata/gui';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormsModule } from '@angular/forms';
+import { MonacoEditorModule } from 'ng-monaco-editor';
 
 @Component({
   standalone: true,
   selector: 'app-playground',
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.scss'],
+  imports: [CommonModule, FormsModule, MonacoEditorModule, GuiModule],
 })
-export class PlaygroundComponent {}
+export class PlaygroundComponent implements OnInit {
+  form = new FormGroup({});
+  config: GuiConfigs = {
+    title: {
+      name: '标题',
+      type: 'text',
+      default: '我是标题',
+    },
+  };
+  model = {};
+  value = {};
+
+  configStr = '';
+
+  ngOnInit(): void {
+    this.configStr = JSON.stringify(this.config, null, 2);
+
+    this.form.valueChanges.subscribe(v => {
+      this.value = v;
+    });
+  }
+
+  onConfigChange() {
+    this.config = JSON.parse(this.configStr);
+    this.model = this.value;
+  }
+}
--- projects/dev-app/src/main.ts
+++ projects/dev-app/src/main.ts
@@ -2,11 +2,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MonacoEditorModule } from 'ng-monaco-editor';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(BrowserModule, BrowserAnimationsModule, HttpClientModule, AppRoutingModule),
+    importProvidersFrom(
+      BrowserModule,
+      BrowserAnimationsModule,
+      HttpClientModule,
+      AppRoutingModule,
+      MonacoEditorModule.forRoot({
+        baseUrl: 'assets/lib',
+      })
+    ),
   ],
 }).catch(err => console.error(err));
