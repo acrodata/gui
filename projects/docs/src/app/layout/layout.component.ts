a1584e91ea8bc93eb9bf5f9035309da42862bced
nzbin <nzb329@163.com>
2023-11-07 22:38:53 +0800

    refactor: rebuild layout and add color tokens

--- projects/docs/src/app/layout/layout.component.ts
+++ projects/docs/src/app/layout/layout.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SidemenuComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, SidemenuComponent],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
   host: {
