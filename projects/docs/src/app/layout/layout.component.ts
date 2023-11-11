630b2e606c94d9be535d8156c4a854ed98927b9d
nzbin <nzb329@163.com>
2023-11-11 22:55:30 +0800

    chore(docs): add features and remove sidemenu component

--- projects/docs/src/app/layout/layout.component.ts
+++ projects/docs/src/app/layout/layout.component.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { SidemenuComponent } from './sidemenu/sidemenu.component';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, SidemenuComponent, MatButtonModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, MatButtonModule],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
   host: {
@@ -15,4 +14,47 @@ import { MatButtonModule } from '@angular/material/button';
   },
   encapsulation: ViewEncapsulation.None,
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  menu = [
+    {
+      name: 'Getting started',
+      route: 'getting-started',
+      children: [],
+    },
+    {
+      name: 'Basic controls',
+      route: 'basic-controls',
+      children: [
+        { name: 'Text', route: 'text' },
+        { name: 'Number', route: 'number' },
+        { name: 'Switch', route: 'switch' },
+        { name: 'Slider', route: 'slider' },
+        { name: 'Fill', route: 'fill' },
+        { name: 'Select', route: 'select' },
+        { name: 'Button Toggle', route: 'button_toggle' },
+        { name: 'Textarea', route: 'textarea' },
+        { name: 'Hidden', route: 'hidden' },
+      ],
+    },
+    {
+      name: 'Group controls',
+      route: 'group-controls',
+      children: [
+        { name: 'Group', route: 'group' },
+        { name: 'Inline Group', route: 'inline_group' },
+        { name: 'Tabs', route: 'tabs' },
+        { name: 'Menu', route: 'menu' },
+      ],
+    },
+    {
+      name: 'Media controls',
+      route: 'media-controls',
+      children: [
+        { name: 'Image', route: 'image' },
+        { name: 'Video', route: 'video' },
+        { name: 'Audio', route: 'audio' },
+        { name: 'File', route: 'file' },
+      ],
+    },
+  ];
+}
