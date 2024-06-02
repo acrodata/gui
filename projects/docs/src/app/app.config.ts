1bf69d188de028f104514b5cdb36e751621cc3e7
nzbin <nzb329@163.com>
2024-06-02 09:05:49 +0800

    chore: update dependencies and small fix

--- projects/docs/src/app/app.config.ts
+++ projects/docs/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
-import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+import { provideHighlightOptions } from 'ngx-highlightjs';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -16,18 +16,15 @@ export const appConfig: ApplicationConfig = {
     ),
     provideAnimations(),
     provideHttpClient(),
-    {
-      provide: HIGHLIGHT_OPTIONS,
-      useValue: {
-        coreLibraryLoader: () => import('highlight.js/lib/core'),
-        languages: {
-          typescript: () => import('highlight.js/lib/languages/typescript'),
-          scss: () => import('highlight.js/lib/languages/scss'),
-          xml: () => import('highlight.js/lib/languages/xml'),
-          json: () => import('highlight.js/lib/languages/json'),
-          diff: () => import('highlight.js/lib/languages/diff'),
-        },
+    provideHighlightOptions({
+      coreLibraryLoader: () => import('highlight.js/lib/core'),
+      languages: {
+        typescript: () => import('highlight.js/lib/languages/typescript'),
+        scss: () => import('highlight.js/lib/languages/scss'),
+        xml: () => import('highlight.js/lib/languages/xml'),
+        json: () => import('highlight.js/lib/languages/json'),
+        diff: () => import('highlight.js/lib/languages/diff'),
       },
-    },
+    }),
   ],
 };
